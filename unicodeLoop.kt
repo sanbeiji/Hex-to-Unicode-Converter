bed179c9dcf1ae4bbd647468598ab8078ef7dae6
Joe Lewis <joelewis@gmail.com>
2023-03-02 19:30:06 -0800

    Update unicodeLoop.kt

--- unicodeLoop.kt
+++ unicodeLoop.kt
@@ -1,14 +1,30 @@
-/**
- * Loop through a "reasonable" subset of Unicode and print the characters
- */
-
 fun main() {
-  for (hexValue in 0x000..0x13430) {
-    print("0x$hexValue: ${ convert(hexValue)}, ")
+  // input a starting and ending value
+  println("Enter a starting number for the counter (less than 149186):")
+  val startNumber = readLine()
+  println("Enter a final number for the counter (no greater than 149186):")
+  val endNumber = readLine()
+
+  // start the timer
+  val start = System.currentTimeMillis()
+
+  // build the unicode map
+  if (startNumber != null && endNumber != null) {
+    print("Running...")
+    for (counter in startNumber.toInt()..endNumber.toInt()) {
+      val hex = Integer.toHexString(counter)
+      println("0x${hex.uppercase()}: " + convertHexToCharacter(hex))
+    }
   }
+
+  // stop the timer
+  val end = System.currentTimeMillis()
+  println("-----")
+  println("Took ${end - start}ms to complete.")
 }
 
-fun convert(hex: Int): String {
-  val decimal = Integer.parseInt(hex.toString(), 16)
-  return Character.toString(decimal)
+// Function to convert hex to characters
+fun convertHexToCharacter(hex: String): Char {
+  val hexValue = Integer.parseInt(hex, 16)
+  return hexValue.toChar()
 }
